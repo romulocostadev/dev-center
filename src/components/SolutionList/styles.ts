7e6e0f2827f9c2f0e12a4b8126712e21c8e051e7
kairo <kairogyn@gmail.com>
2022-06-23 10:08:16 -0300

    auth atualizado

--- src/components/SolutionList/styles.ts
+++ src/components/SolutionList/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MySolutions = styled.div`
   height: 240px;
-  width: 1200px;
+  max-width: 1200px;
   display: flex;
   box-sizing: border-box;
   gap: 24px;
